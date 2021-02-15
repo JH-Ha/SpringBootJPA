4222d3244719c3a8779b3386c7d6847f9611b4ca
Jaehyeong Ha <cubeprince@gmail.com>
2021-02-16 00:12:56 +0900

    add get members api

--- src/main/java/jpabook/jpashop/api/MemberApiControoler.java
+++ src/main/java/jpabook/jpashop/api/MemberApiControoler.java
@@ -9,12 +9,40 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
 public class MemberApiControoler {
     private final MemberService memberService;
 
+    @GetMapping("/api/v1/members")
+    public List<Member> membersV1(){
+        return memberService.findMembers();
+    }
+
+    @GetMapping("/api/v2/members")
+    public Result membersV2(){
+        List<Member> findMembers = memberService.findMembers();
+        List<MemberDto> collect = findMembers.stream()
+                .map(m -> new MemberDto(m.getName()))
+                .collect(Collectors.toList());
+        return new Result(collect.size(), collect);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T>{
+        private int count;
+        private T data;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto{
+        private String name;
+    }
     /* 절대 Entity를 외부에 노출해서는 안된다.
     * 잘못된 API의 예
     * 1. Entity가 바뀜으로 인해서 API Spec이 변화될 가능성이 있다.
