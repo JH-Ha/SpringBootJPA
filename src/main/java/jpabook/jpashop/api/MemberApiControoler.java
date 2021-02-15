3f7e3d30acf83959c51fbd3d9944bebce423c0ef
Jaehyeong Ha <cubeprince@gmail.com>
2021-02-15 23:55:57 +0900

    add update member api

--- src/main/java/jpabook/jpashop/api/MemberApiControoler.java
+++ src/main/java/jpabook/jpashop/api/MemberApiControoler.java
@@ -2,13 +2,11 @@ package jpabook.jpashop.api;
 
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.service.MemberService;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -35,6 +33,15 @@ public class MemberApiControoler {
         Long id = memberService.join(member);
         return new CreateMemberResponse(id);
     }
+    @PutMapping("/api/v2/members/{id}")
+    public UpdateMemberResponse updateMemberV2(@PathVariable("id") Long id,
+                                               @RequestBody @Valid UpdateMemberRequest request){
+        //command와 query를 분리해서 개발
+        memberService.update(id, request.getName());
+        Member findMember = memberService.findOne(id);
+        return new UpdateMemberResponse(findMember.getId(), findMember.getName());
+    }
+
     @Data
     static class CreateMemberRequest{
         private String name;
@@ -45,5 +52,15 @@ public class MemberApiControoler {
         public CreateMemberResponse(Long id){
             this.id = id;
         }
+    }
+    @Data
+    static class UpdateMemberRequest{
+        private String name;
+    }
+    @Data
+    @AllArgsConstructor
+    static class UpdateMemberResponse {
+        private Long id;
+        private String name;
     }
 }
